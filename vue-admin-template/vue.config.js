bdcfe97730151d0761218dca74e1f80c9fa4232b
18120574956 <2751470984@qq.com>
2026-06-28 14:58:56 +0800

    ``` feat(config): 添加开发环境API代理配置
    为解决前端开发环境中的跨域问题，配置了API代理规则。
    将 '/dev-api' 路径代理到后端服务器地址 http://192.168.0.104:8084，
    并启用 changeOrigin 选项以允许跨域请求。同时添加路径重写规则，
    将请求中的 '/dev-api' 前缀替换为 '/' 以匹配后端实际接口路径。
    ```

--- vue-admin-template/vue.config.js
+++ vue-admin-template/vue.config.js
@@ -36,6 +36,16 @@ module.exports = {
       warnings: false,
       errors: true
     },
+    proxy: {
+      '/dev-api': {
+        target: 'http://192.168.0.104:8084/', // 后端 API 地址
+        changeOrigin: true, // 允许跨域
+        pathRewrite: {
+          '^/dev-api': '/' // 把 '/dev-api' 重写为空或替换为后端实际路径
+        },
+      },
+    },
+    disableHostCheck: true,
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
